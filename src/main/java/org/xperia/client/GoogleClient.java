f7f3676de23cefecc61c2862f9d946bb4768104c
Emil <emilpradeep@gmail.com>
2026-09-05 23:08:49 +0530

    feat: added google response models

--- src/main/java/org/xperia/client/GoogleClient.java
+++ src/main/java/org/xperia/client/GoogleClient.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.xperia.models.GoogleTokenResponse;
+import org.xperia.models.google.GoogleTokenResponse;
 
 @Component
 public class GoogleClient {
@@ -18,6 +18,34 @@ public class GoogleClient {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     * The function fetches all labels of the user's gmail account. The response would contain list of labels and their corresponding id's
+     * @param accessToken The google oauth token of the user
+     * @return list of labels and their ids in string format
+     */
+    public String getLabelIds(String accessToken){
+
+        String url = "https://gmail.googleapis.com/gmail/v1/users/me/labels";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(accessToken);
+
+        HttpEntity<Void> request = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> response =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        request,
+                        String.class
+                );
+        return response.getBody();
+    }
+
+    /**
+     * The function fetches all the gmail messages based on the labelId provided
+     * @param accessToken The google oauth token of the user
+     * @param labelId The labelId based on which the gmail messages needs to be fetched
+     * @return The list of messages (message id's) in string format
+     */
     public String getMessages(String accessToken, String labelId){
 
         String url = "https://gmail.googleapis.com/gmail/v1/users/me/messages" +
